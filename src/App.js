fee14da72f69e8bbbe24e6d9292a9f8fdf840216
Gabriel <ggauto994@gmail.com>
2022-05-08 18:59:51 -0400

    terminada la clasa y los dos desafios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React from "react";
+import React, { Fragment } from "react";
 import Testimonio from "./componentes/testimonio";
 
 function App() {
@@ -7,7 +7,70 @@ function App() {
     <div className="App">
       <div className="contenedor-principal">
         <h1>Esto es lo que dicen nuestros clientes:</h1>
-        <Testimonio></Testimonio>
+        <Testimonio
+          key="1"
+          img="woman"
+          nombre="Emma Bostian"
+          pais="Suecia"
+          cargo="Ingeniera de Software"
+          empresa="Spotify"
+          testimonio={
+            <>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat . Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur.{" "}
+              <strong> Excepteur sint occaecat cupidatat non proident</strong>,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </>
+          }
+        ></Testimonio>
+        <Testimonio
+          key="2"
+          img="woman1"
+          nombre="Olga Lee"
+          pais="Noruega"
+          cargo="Ingeniera de Software"
+          empresa="Amazon"
+          testimonio={
+            <>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam,
+              <strong>
+                {" "}
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                commodo consequat
+              </strong>
+              . Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit
+              anim id est laborum.
+            </>
+          }
+        ></Testimonio>
+        <Testimonio
+          key="3"
+          img="woman2"
+          nombre="Sofía Llanos"
+          pais="España"
+          cargo="Ingeniera de Software"
+          empresa="OnlyFans"
+          testimonio={
+            <>
+              <strong>Lorem ipsum dolor sit amet</strong>, consectetur
+              adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate
+              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+              occaecat cupidatat non proident, sunt in culpa qui officia
+              deserunt mollit anim id est laborum.
+            </>
+          }
+        ></Testimonio>
       </div>
     </div>
   );
@@ -15,4 +78,10 @@ function App() {
 
 export default App;
 
-//DOS HORAS 10 MIN
+/* RETOS:
+  
+  -HACER LOS PROPS EN UN OBJETO SEPARADO Y MAPEARLO
+  -HACER RESPONSIVO
+*/
+
+//02:48:00
--- src/componentes/testimonio.js
+++ src/componentes/testimonio.js
@@ -1,28 +1,25 @@
 import React from "react";
 import "../css/testimonio.css";
 
-export default function Testimonio() {
+export default function Testimonio(props) {
   return (
     <div className="contenedor-testimonio">
       <figure className="fig-testimonio">
         <img
+          loading="lazy"
           className="img-testimonio"
-          src={require("../imagenes/woman2.jpg")}
-          alt="testimonio"
+          src={require(`../imagenes/${props.img}.jpg`)}
+          alt={props.img}
         ></img>
       </figure>
       <div className="contenedor-texto-testimonio">
-        <p className="nombre-testimonio">Emma Bostian en Suecia</p>
-        <p className="cargo-testimonio">Ingeniera de Software en Spotify</p>
-        <p className="texto-testimonio">
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum."
+        <p className="nombre-testimonio">
+          <strong> {props.nombre}</strong> en {props.pais}
         </p>
+        <p className="cargo-testimonio">
+          {props.cargo} en <strong>{props.empresa}</strong>
+        </p>
+        <p className="texto-testimonio">{props.testimonio}</p>
       </div>
     </div>
   );
